49246c1843878ced120933939a7bf1d56a32beb3
PodleshyySergey <60898756+PodleshyySergey@users.noreply.github.com>
2020-12-03 11:03:49 +0300

    Add test "testYandexSearch"

--- src/test/java/tests/SearchEnginesTests.java
+++ src/test/java/tests/SearchEnginesTests.java
@@ -1,9 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,6 +17,17 @@ public class SearchEnginesTests {
         open("https://www.google.com");
         $(byName("q")).setValue("Selenide").pressEnter();
         $("html").shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    public void testYandexSearch() {
+        Configuration.startMaximized = true;
+        Configuration.browser = "edge";
+        open("https://yandex.ru/");
+        $("#text").val("selenide");
+        $(byText("Найти")).click();
+
+        $(byText("ru.selenide.org")).shouldBe(visible);
     }
 
 }
